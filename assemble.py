7440ffcb4c7fe55f7370ebbd63579b76a0433af5
Kartik Ayyer <kartik.ayyer@desy.de>
2019-07-15 16:16:26 +0200

    assemble: cleaned up class definition

--- assemble.py
+++ assemble.py
@@ -11,26 +11,22 @@ from PIL import Image
 import pyqtgraph as pg
 matplotlib.use('QT5Agg')
 
-class assemble(self,my_path,step):
-    
-    def __init__(self):
-
+class Assembler():
+    def __init__(self, my_path, step=12):
         self.step = step
         self.path = my_path
-        
 
     def assemble(self):
-    
-        f = mrc.open(self.path,'r',permissive=True)
+        f = mrc.open(self.path, 'r', permissive=True)
         h = f.header
-        data = f.data[:,::10,::10]
-        dimensions=data.shape
+        data = f.data[:,::self.step,::self.step]
+        dimensions = data.shape
 
         eh = np.frombuffer(f.extended_header,dtype='i2')
 
-        pos_x = eh[1:self.step*dimensions[0]:self.step]//10
-        pos_y = eh[2:self.step*dimensions[0]:self.step]//10
-        pos_z = eh[3:self.step*dimensions[0]:self.step]
+        pos_x = eh[1:10*dimensions[0]:10] // self.step
+        pos_y = eh[2:10*dimensions[0]:10] // self.step
+        pos_z = eh[3:10*dimensions[0]:10]
 
         cy, cx = np.indices(dimensions[1:3])
 
@@ -50,17 +46,3 @@ if __name__=='__main__':
     path = '../gs.mrc'
     merged = assemble(path)
     pg.show(merged.T)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
