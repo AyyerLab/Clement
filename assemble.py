e60652e4fa8b52c2bfcd7949dde4adef9f880075
Kartik Ayyer <kartik.ayyer@desy.de>
2019-07-15 16:59:46 +0200

    assemble: Refactored class to allow saving and possibly other operations on data

--- assemble.py
+++ assemble.py
@@ -12,37 +12,44 @@ import pyqtgraph as pg
 matplotlib.use('QT5Agg')
 
 class Assembler():
-    def __init__(self, my_path, step=12):
+    def __init__(self, step=12):
         self.step = step
-        self.path = my_path
+
+    def parse(self, fname):
+        with mrc.open(fname, 'r', permissive=True) as f:
+            self.data = f.data[:,::self.step,::self.step]
+            self._h = f.header
+            self._eh = np.frombuffer(f.extended_header, dtype='i2')
 
     def assemble(self):
-        f = mrc.open(self.path, 'r', permissive=True)
-        h = f.header
-        data = f.data[:,::self.step,::self.step]
-        dimensions = data.shape
+        dimensions = self.data.shape
 
-        eh = np.frombuffer(f.extended_header,dtype='i2')
-
-        pos_x = eh[1:10*dimensions[0]:10] // self.step
-        pos_y = eh[2:10*dimensions[0]:10] // self.step
-        pos_z = eh[3:10*dimensions[0]:10]
+        pos_x = self._eh[1:10*dimensions[0]:10] // self.step
+        pos_y = self._eh[2:10*dimensions[0]:10] // self.step
+        pos_z = self._eh[3:10*dimensions[0]:10]
 
         cy, cx = np.indices(dimensions[1:3])
 
-        merged = np.zeros((np.max(pos_x)+dimensions[2],np.max(pos_y)+dimensions[1]))
-        counts = np.zeros_like(merged)
+        self.merged = np.zeros((np.max(pos_x)+dimensions[2],np.max(pos_y)+dimensions[1]), dtype='f4')
+        self.mcounts = np.zeros_like(self.merged)
         for i in range(dimensions[0]):
         #for i in range(5):
             print('Merge for image {}'.format(i))
-            np.add.at(counts,(cx+pos_x[i],cy+pos_y[i]),1)
-            np.add.at(merged,(cx+pos_x[i],cy+pos_y[i]),data[i])
+            np.add.at(self.mcounts, (cx+pos_x[i], cy+pos_y[i]), 1)
+            np.add.at(self.merged, (cx+pos_x[i], cy+pos_y[i]), self.data[i])
 
-        merged[counts>0] /= counts[counts>0]
+        self.merged[self.mcounts>0] /= self.mcounts[self.mcounts>0]
 
-        return merged
+        return self.merged
+
+    def save_merge(self, fname):
+        with mrc.new(fname, overwrite=True) as f:
+            f.set_data(self.merged)
+            f.update_header_stats()
 
 if __name__=='__main__':
     path = '../gs.mrc'
-    merged = assemble(path)
+    assembler = Assembler()
+    assembler.parse(path)
+    assembler.assemble()
     pg.show(merged.T)
