706a90237ea998ff80125553d33144e0f1025e8d
Kartik Ayyer <kartik.ayyer@desy.de>
2019-09-07 14:48:37 +0200

    setup: Updated after getting running on Windows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fptr:
 
 setuptools.setup(
     name='clement',
-    version='0.1.2',
+    version='0.1.4',
     author='CNI Group at MPSD',
     author_email='kartik.ayyer@mpsd.mpg.de',
     description='GUI for Correlative Light and Electron Microscopy',
