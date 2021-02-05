Correct error calculation and 3D correlation

clement/base_controls.py
@@ -477,20 +477,25 @@ class BaseControls(QtWidgets.QWidget):
         return init
 
     def _calc_optimized_position(self, point, pos=None):
+        print(1)
         peaks = None
         ind = self.ops.check_peak_index(point, self.size, transformed=self.ops._transformed)
         if ind is None and self.other.tab_index == 1 and not self.other._refined:
             self.print('If the FIB tab is selected, you have to select a bead for the first refinement!')
+            print(2)
             return None, None, None
         if ind is not None:
+            print(3)
             if self.ops._transformed:
                 peaks = self.ops.tf_peak_slices[-1]
             else:
                 peaks = self.ops.peak_slices[-1]
         z = self.ops.calc_z(ind, point, self.ops._transformed, self.point_ref_btn.currentIndex())
         if z is None:
+            print(4)
             self.print('z is None, something went wrong here... Try another bead!')
             return None, None, None
+        print(5)
         self.points_corr_z.append(z)
 
         if ind is not None:
@@ -1382,6 +1387,7 @@ class BaseControls(QtWidgets.QWidget):
         self.other._orig_points_corr = []
 
     def _undo_refinement(self):
+        print(len(self.other._points_corr_history))
         self.other.ops.undo_refinement()
         for i in range(len(self._points_corr)):
             self._remove_correlated_points(self._points_corr[0])
@@ -1404,14 +1410,12 @@ class BaseControls(QtWidgets.QWidget):
             self.other._recalc_grid()
             self.other._points_corr_indices = np.arange(len(self.other._points_corr)).tolist()
 
-            idx = self.other.tab_index
-            self._estimate_precision(idx, self.other.ops._refine_matrix)
-            self.other.size = copy.copy(self.size)
-
         self._update_tr_matrices()
         #self.select_btn.setChecked(True)
         self.other.size = copy.copy(self.other._size_history[-1])
+        print('heeere')
         for i in range(len(self._points_corr_history[-1])):
+            print(i)
             point = self._points_corr_history[-1][i]
             self._draw_correlated_points(point.pos(), self.imview.getImageItem())
             point_other = self.other._points_corr_history[-1][i]
@@ -1423,6 +1427,13 @@ class BaseControls(QtWidgets.QWidget):
             self.other.show_peaks_btn.setChecked(True)
 
         self.other.ops.merged[self.other.tab_index] = None
+
+        print(len(self.other._points_corr_history))
+        if len(self.other.ops._refine_history) != 0:
+            idx = self.other.tab_index
+            self._estimate_precision(idx, self.other.ops._refine_matrix)
+            self.other.size = copy.copy(self.size)
+
         id = len(self._fib_vs_sem_history) - self._fib_vs_sem_history[::-1].index(self.other.tab_index) - 1
 
         del self._fib_vs_sem_history[id]

clement/em_operations.py
@@ -344,19 +344,13 @@ class EM_ops():
             self._tf_points_region = np.copy(pts)
         self.toggle_original()
 
-    def calc_fib_transform(self, delta_sigma, sem_shape, sem_pixel_size, shift=np.zeros(2), sem_transpose=False):
+    def calc_fib_transform(self, delta_sigma, sem_shape, fm_voxel, sem_pixel_size, shift=np.zeros(2), sem_transpose=False):
         if self.box_shift is not None:
             shift = self.box_shift - shift
         if sem_transpose:
             self.fib_matrix = np.array([[0., 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
         else:
             self.fib_matrix = np.identity(4)
-
-        # rotate by phi angle in plane
-        self.fib_matrix = np.array([[1, 0, 0, 0],
-                                    [0, 1, 0, 0],
-                                    [0, 0, 1, 0],
-                                    [0, 0, 0, 1]]) @ self.fib_matrix
 
         # Scale according to pixel sizes
         scale = sem_pixel_size[:2] / self.pixel_size[:2]
@@ -365,7 +359,8 @@ class EM_ops():
         self.log('Scale: ', scale)
         self.fib_matrix = np.array([[scale[0], 0, 0, 0],
                                     [0, scale[1], 0, 0],
-                                    [0, 0, 1, 0],
+                                    #[0, 0, 1, 0],
+                                    [0, 0, fm_voxel[2]*1e9/self.pixel_size[0], 0],
                                     [0, 0, 0, 1]]) @ self.fib_matrix
 
         # Rotate SEM image according to sigma angles
@@ -383,6 +378,12 @@ class EM_ops():
         self.fib_matrix[:3, 3] += self.fib_shift
         self.fib_matrix[:2, 3] -= shift
         self.log('Shifted fib matrix: ', self.fib_matrix)
+
+        p1 = np.array([400,600,0,1])
+        p2 = np.array([400,600,25*fm_voxel[2]/fm_voxel[0],1])
+        print('Test p1: ', self.fib_matrix @ p1)
+        print('Test p2: ', self.fib_matrix @ p2)
+
 
     def apply_fib_transform(self, points, num_slices, scaling=1):
         self.log('Num slices: ', num_slices)
@@ -672,7 +673,7 @@ class EM_ops():
                 [[0, 1, 0], [1, 0, 0], [0, 0, 1]]) @ rot_matrix
 
         p0 = np.array([0, 0, 0, 1])
-        p1 = np.array([0, 0, voxel_size[2] / voxel_size[0], 1])
+        p1 = np.array([0, 0, 1, 1])
         self.z_shift = (self.fib_matrix @ p1)[:2] - (self.fib_matrix @ p0)[:2]
         self.log('z_shift: ', self.z_shift)
 
@@ -683,8 +684,6 @@ class EM_ops():
         tf_matrix = np.copy(tf_matrix_fm)
         tf_matrix[:2, 2] += tf_corners_fm.min(1)[:2]
 
-        if self._refine_matrix is None:
-            refine_matrix = np.identity(3)
         total_matrix = self._refine_matrix @ fib_2d @ corr_matrix @ rot_matrix @ tf_matrix
 
         nx, ny = fm_data_orig.shape[:2]
@@ -698,7 +697,7 @@ class EM_ops():
             for i in range(len(orig_points)):
                 img_tmp = np.zeros([nx, ny])
                 img_tmp[int(np.round(orig_points[i][0])), int(np.round(orig_points[i][1]))] = 1
-                z = fm_z_values[i] / (voxel_size[2] / voxel_size[0])
+                z = fm_z_values[i]
                 fib_new = np.copy(fib_2d)
                 fib_new[:2, 2] += z * self.z_shift
                 shift_matrix = self._refine_matrix @ fib_new @ corr_matrix @ rot_matrix @ tf_matrix @ color_matrices[0]

clement/fib_controls.py
@@ -149,7 +149,7 @@ class FIBControls(BaseControls):
             is_transposed = self.transp_btn.isChecked()
 
             self.ops.calc_fib_transform(sigma_angle, self.sem_ops.data.shape,
-                                        self.sem_ops.pixel_size, shift=shift, sem_transpose=is_transposed)
+                                        self.other.ops.voxel_size, self.sem_ops.pixel_size, shift=shift, sem_transpose=is_transposed)
             self.ops.apply_fib_transform(self.sem_ops._orig_points, self.num_slices, scaling)
 
         if self.ops.points is not None:
@@ -161,7 +161,7 @@ class FIBControls(BaseControls):
                 self.old_pos0 = [0, 0]
             self.grid_box.sigRegionChangeFinished.connect(self._update_shifts)
             if redo:
-                self.ops.calc_fib_transform(sigma_angle, self.sem_ops.data.shape,
+                self.ops.calc_fib_transform(sigma_angle, self.sem_ops.data.shape, self.other.ops.voxel_size,
                                             self.sem_ops.pixel_size, shift=np.zeros(2), sem_transpose=is_transposed)
                 self.ops.apply_fib_transform(self.sem_ops._orig_points, self.num_slices, scaling)
                 pos = list(self.ops.points)
@@ -177,6 +177,7 @@ class FIBControls(BaseControls):
                 self.imview.addItem(self.grid_box)
             self.show_peaks_btn.setEnabled(True)
             self.auto_opt_btn.setEnabled(True)
+            self.size_box.setEnabled(True)
             if self.show_peaks_btn.isChecked():
                 self.show_peaks_btn.setChecked(False)
                 self.show_peaks_btn.setChecked(True)

clement/fm_operations.py
@@ -102,6 +102,7 @@ class FM_ops(Peak_finding):
                 self.print('Voxel size: ', self.voxel_size)
                 self.old_fname = fname
 
+
             # TODO: Look into modifying read_lif to get
             # a single Z-slice with all channels rather than all slices for a single channel
             self.orig_data = np.array([self.reader.getFrame(channel=i, dtype='u2')[z, :, :].astype('f4')
@@ -351,10 +352,8 @@ class FM_ops(Peak_finding):
         if z is None:
             self.print('Oops, something went wrong. Try somewhere else!')
             return None
-        # flip z axis and scale by voxel size ratio
+        # flip z axis
         z = self.num_slices - 1 - z
-        scaling = self.voxel_size[2] / self.voxel_size[0]
-        z *= scaling
         return z
 
     def load_channel(self, ind):

clement/popup.py
@@ -11,7 +11,6 @@ from skimage.color import hsv2rgb
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator, FormatStrFormatter
-from uncertainties import unumpy
 warnings.simplefilter('ignore', category=FutureWarning)
 
 from . import utils
@@ -791,16 +790,13 @@ class Merge(QtGui.QMainWindow):
     def _calc_ellipses_popup(self, cov):
         cov_matrix = np.copy(self.parent.fm_controls.other.cov_matrix)
         self.log('Cov matrix: \n', cov_matrix)
-        self.log('Cov_i matrix: \n', cov)
+        self.log('Cov_i matrix: \n', cov[:2,:2]*(self.pixel_size[0]**2))
         self.print('Fitting precision in merged frame: ', np.sqrt(np.diag(cov[:2, :2])) * self.pixel_size[0])
-        print('Correlation covariance: \n', cov_matrix)
-        print('Transformed cov_i matrix: \n', cov[:2,:2] * (self.pixel_size[0])**2)
         cov_matrix += (cov[:2, :2] * (self.pixel_size[0]**2))
 
-        print('Total covariance: \n', cov_matrix)
-        print('Total transformed errors: ', np.sqrt(np.diag(cov_matrix)))
+        self.log('Total covariance: \n', cov_matrix)
+        self.log('Total transformed errors [nm]: ', np.sqrt(np.diag(cov_matrix)))
         eigvals, eigvecs = np.linalg.eigh(cov_matrix)
-        print('Eigenvalues: ', eigvals)
 
         order = eigvals.argsort()[::-1]
         eigvals, eigvecs = eigvals[order], eigvecs[order]
@@ -810,27 +806,24 @@ class Merge(QtGui.QMainWindow):
         #scale eigenvalues to 75% confidence interval and pixel size
         #lambda_1, lambda_2 = 2 * np.sqrt(2.77*eigvals)[:2] / np.array(self.pixel_size[:2])
         #scale eigenvalues to 68.3% (1sigma) confidence interval and pixel size
-        #lambda_1, lambda_2 = 2 * np.sqrt(2.3*eigvals)[:2] / np.array(self.pixel_size[:2])
+        lambda_1, lambda_2 = 2 * np.sqrt(2.3*eigvals)[:2] / np.array(self.pixel_size[:2])
         #scale eigenvalues to 50%  confidence interval and pixel size
-        lambda_1, lambda_2 = 2 * np.sqrt(1.39*eigvals)[:2] / np.array(self.pixel_size[:2])
+        #lambda_1, lambda_2 = 2 * np.sqrt(1.39*eigvals)[:2] / np.array(self.pixel_size[:2])
         self.log(lambda_1, lambda_2)
-        print('Lambdas [nm]: ', lambda_1*self.pixel_size[0], lambda_2*self.pixel_size[0])
+        self.print('Lambdas [nm]: ', lambda_1*self.pixel_size[0], lambda_2*self.pixel_size[0])
         return lambda_1, lambda_2, theta
 
     def _convert_pois(self, points=None):
         if points is None:
             points = [p.pos() for p in self.parent.fm_controls.pois]
             z_values = self.parent.fm_controls.pois_z
-            errs = self.parent.fm_controls.pois_err
             covs = self.parent.fm_controls.pois_cov
             sizes = self.parent.fm_controls.pois_sizes
         else:
             z_values = [self.parent.fm_controls.pois_z[-1]]
-            errs = [self.parent.fm_controls.pois_err[-1]]
             covs = [self.parent.fm_controls.pois_cov[-1]]
             sizes = [self.parent.fm_controls.pois_sizes[-1]]
         transf_points = []
-        transf_err = []
         transf_covs = []
         if self.other.tab_index == 1:
             tr_matrix = np.copy(self.other.tr_matrices)
@@ -841,41 +834,31 @@ class Merge(QtGui.QMainWindow):
             tot_matrix = refine_matrix @ self.other.ops.fib_matrix @ tr_matrix
             for i in range(len(points)):
                 pos = points[i]
-                std = errs[i]
                 size = sizes[i]
-                scaling = self.parent.fm_controls.ops.voxel_size[2] / self.parent.fm_controls.ops.voxel_size[0]
-                point = unumpy.uarray([pos.x() + size[0] // 2, pos.y() + size[1] // 2, z_values[i], 1], [std[0], std[1], std[2]*scaling, 0])
+                point = np.array([pos.x() + size[0] // 2, pos.y() + size[1] // 2, z_values[i], 1])
                 transf = tot_matrix @ point
-                transf_points.append(unumpy.nominal_values(transf)[:2])
-                transf_err.append(unumpy.std_devs(transf)[:2])
+                transf_points.append(transf[:2])
                 cov_i = np.insert(np.insert(np.array(covs[i]), 3, 0, axis=0), 3, 0, axis=1)
-                cov_i[:,-2] *= scaling**2
-                cov_i[-2,:] *= scaling**2
                 tf_cov = tot_matrix @ np.array(cov_i) @ tot_matrix.T
-                perr = np.sqrt(np.diag(tf_cov))
-                print('Original fitting errors {} [nm]: '.format(i), errs[i]*self.parent.fm_controls.ops.voxel_size*1e9)
-                print('Transformed fitting errors {} [nm]: '.format(i), transf_err[-1]*self.other.ops.pixel_size)
                 transf_covs.append(tf_cov)
         else:
             tot_matrix = np.linalg.inv(self.other.ops.tf_matrix) @ self.other.tr_matrices
             for i in range(len(points)):
                 pos = points[i]
-                std = errs[i][:2]
                 size = sizes[i]
-                point = unumpy.uarray([pos.x() + size[0] // 2, pos.y() + size[1] // 2, 1], [std[0], std[1], 0])
+                point = np.array([pos.x() + size[0] // 2, pos.y() + size[1] // 2, 1])
                 transf = tot_matrix @ point
-                transf_points.append(unumpy.nominal_values(transf)[:2] / self.downsampling)
-                transf_err.append(unumpy.std_devs(transf)[:2])
+                transf_points.append(transf[:2] / self.downsampling)
                 cov_i = np.array(covs[i])
                 cov_i[-1,-1] = 0
                 tf_cov = tot_matrix @ cov_i @ tot_matrix.T
                 transf_covs.append(tf_cov[:3, :3])
 
-        return transf_points, transf_err, transf_covs
+        return transf_points, transf_covs
 
     @utils.wait_cursor('print')
     def _copy_pois(self, state=None):
-        tf_points, tf_errs, tf_covs = self._convert_pois()
+        tf_points, tf_covs = self._convert_pois()
         for i in range(len(tf_points)):
             pos = QtCore.QPointF(tf_points[i][0], tf_points[i][1])
             self._draw_correlated_points_popup(pos, self.imview_popup.getImageItem(), tf_covs[i])
@@ -883,7 +866,7 @@ class Merge(QtGui.QMainWindow):
 
     @utils.wait_cursor('print')
     def _update_poi(self, pos):
-        tf_points, tf_errs, tf_covs = self._convert_pois([pos])
+        tf_points, tf_covs = self._convert_pois([pos])
         pos = QtCore.QPointF(tf_points[0][0], tf_points[0][1])
         self._draw_correlated_points_popup(pos, self.imview_popup.getImageItem(), tf_covs[0])
         self._clicked_points_popup_base_indices.append(self.counter_popup-1)

clement/project.py
@@ -265,12 +265,12 @@ class Project(QtWidgets.QWidget):
 
         try:
             self.fib.ops._orig_points = np.array(fibdict['Original points'])
-            self.fib.show_grid_btn.setChecked(True)
-            if 'Box shift' in fibdict:
-                pos = self.fib.grid_box.pos()
-                shift = fibdict['Box shift']
-                new_pos = [pos.x() + shift[0], pos.y() + shift[1]]
-                self.fib.grid_box.setPos(QtCore.QPointF(new_pos[0], new_pos[1]))
+            #self.fib.show_grid_btn.setChecked(True)
+            #if 'Box shift' in fibdict:
+            #    pos = self.fib.grid_box.pos()
+            #    shift = fibdict['Box shift']
+            #    new_pos = [pos.x() + shift[0], pos.y() + shift[1]]
+            #    self.fib.grid_box.setPos(QtCore.QPointF(new_pos[0], new_pos[1]))
             self.fib.show_grid_btn.setChecked(fibdict['Show grid'])
             #self.fib._recalc_grid()
         except KeyError:
@@ -578,8 +578,8 @@ class Project(QtWidgets.QWidget):
         fibdict['Show grid'] = self.fib.show_grid_btn.isChecked()
         if self.fib.ops._orig_points is not None:
             fibdict['Original points'] = self.fib.ops._orig_points.tolist()
-        if self.fib.box_shift is not None:
-            fibdict['Box shift'] = self.fib.box_shift.tolist()
+        #if self.fib.box_shift is not None:
+        #    fibdict['Box shift'] = self.fib.box_shift.tolist()
         if self.fib.ops._total_shift is not None:
             fibdict['Total shift'] = self.fib.ops._total_shift.tolist() if self.fib.ops._total_shift is not None else [
                 0.0, 0.0]