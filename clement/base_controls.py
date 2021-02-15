Correct FM transpose, flip z axis when loading FM, z=0 at the top, do clockwise rotation about x

clement/base_controls.py
@@ -379,6 +379,8 @@ class BaseControls(QtWidgets.QWidget):
             self._transform_pois(init[:2], poi=True)
 
         self._draw_fm_pois(init[:2], item)
+        #if len(self.pois_z) > 0:
+        #    self._draw_correlated_points(QtCore.QPointF(init[0]-self.size/2, init[1]-self.size/2), self.imview.getImageItem(), skip=True)
 
         if self.other.show_merge:
             self.other.popup._update_poi(pos)
@@ -461,9 +463,13 @@ class BaseControls(QtWidgets.QWidget):
 
         point_obj.sigRemoveRequested.connect(lambda: self._remove_pois(point_obj))
 
-    def _draw_correlated_points(self, pos, item):
+    def _draw_correlated_points(self, pos, item, skip=False):
         point = np.array([pos.x() + self.size / 2, pos.y() + self.size / 2])
-        init, pos, z = self._calc_optimized_position(point, pos)
+        if not skip:
+            init, pos, z = self._calc_optimized_position(point, pos)
+        else:
+            init = np.array([point[0], point[1], 1])
+            z = self.pois_z[-1]
         if init is None:
             return
         init_base = copy.copy(init)
@@ -1245,7 +1251,7 @@ class BaseControls(QtWidgets.QWidget):
                     if self.tab_index != 1:
                         self.show_peaks_btn.setEnabled(True)
 
-                elif not hasattr(self, 'tab_index') and self.other.tab_index != 1:
+                elif not hasattr(self, 'tab_index'):
                     self.other.show_peaks_btn.setEnabled(True)
 
     def _show_original(self):

clement/em_operations.py
@@ -348,6 +348,7 @@ class EM_ops():
     def calc_fib_transform(self, delta_sigma, sem_shape, fm_voxel, sem_pixel_size, shift=np.zeros(2), sem_transpose=False):
         if self.box_shift is not None:
             shift = self.box_shift - shift
+
         if sem_transpose:
             self.fib_matrix = np.array([[0., 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
         else:
@@ -360,17 +361,19 @@ class EM_ops():
         self.log('Scale: ', scale)
         self.fib_matrix = np.array([[scale[0], 0, 0, 0],
                                     [0, scale[1], 0, 0],
-                                    #[0, 0, 1, 0],
-                                    [0, 0, fm_voxel[2]*1e9/self.pixel_size[0], 0],
+                                    [0, 0, -fm_voxel[2]*1e9/self.pixel_size[0], 0], #z=0 is at the top, z slices shifted downwards in FIB image
                                     [0, 0, 0, 1]]) @ self.fib_matrix
 
         # Rotate SEM image according to sigma angles
         self.fib_angle = delta_sigma + 52
-        total_angle = self.fib_angle * np.pi / 180
+        total_angle = -self.fib_angle * np.pi / 180 #right-hand rule, clockwise rotation
+
+        #Rx
         self.fib_matrix = np.array([[1, 0, 0, 0],
                                     [0, np.cos(total_angle), -np.sin(total_angle), 0],
                                     [0, np.sin(total_angle), np.cos(total_angle), 0],
                                     [0, 0, 0, 1]]) @ self.fib_matrix
+
 
         nx, ny = sem_shape
         corners = np.array([[0, 0, 0, 1], [nx, 0, 0, 1], [nx, ny, 0, 1], [0, ny, 0, 1]]).T
@@ -390,7 +393,7 @@ class EM_ops():
         src = np.zeros((points.shape[0], 4))
         dst = np.zeros_like(src)
         for i in range(points.shape[0]):
-            src[i, :] = [points[i, 0], points[i, 1], 12, 1] #z == 12 is an estimate for the z position of the sem grid square
+            src[i, :] = [points[i, 0], points[i, 1], 0, 1] #z == 10 is an estimate for the z position of the sem grid square
             dst[i, :] = self.fib_matrix @ src[i, :]
             if self._refine_matrix is not None:
                 dst[i, :3] = self._refine_matrix @ np.array([dst[i, 0], dst[i, 1], 1])

clement/fib_controls.py
@@ -149,7 +149,7 @@ class FIBControls(BaseControls):
 
     @utils.wait_cursor('print')
     def _recalc_grid(self, state=None, recalc_matrix=True, scaling=1, shift=np.array([0,0])):
-        if self.sem_ops is not None and recalc_matrix:
+        if self.sem_ops is not None and self.sem_ops.points is not None and recalc_matrix:
             if self.box_shift is None:
                 shift = np.zeros(2)
                 redo = True
@@ -158,10 +158,10 @@ class FIBControls(BaseControls):
                 redo = False
 
             sigma_angle = float(self.sigma_btn.text())
-            is_transposed = self.transp_btn.isChecked()
+            #is_transposed = not self.transp_btn.isChecked()
 
             self.ops.calc_fib_transform(sigma_angle, self.sem_ops.data.shape,
-                                        self.other.ops.voxel_size, self.sem_ops.pixel_size, shift=shift, sem_transpose=is_transposed)
+                                        self.other.ops.voxel_size, self.sem_ops.pixel_size, shift=shift, sem_transpose=False)
             self.ops.apply_fib_transform(self.sem_ops._orig_points, self.num_slices, scaling)
 
         if self.ops.points is not None:
@@ -174,7 +174,7 @@ class FIBControls(BaseControls):
             self.grid_box.sigRegionChangeFinished.connect(self._update_shifts)
             if redo:
                 self.ops.calc_fib_transform(sigma_angle, self.sem_ops.data.shape, self.other.ops.voxel_size,
-                                            self.sem_ops.pixel_size, shift=np.zeros(2), sem_transpose=is_transposed)
+                                            self.sem_ops.pixel_size, shift=np.zeros(2), sem_transpose=False)
                 self.ops.apply_fib_transform(self.sem_ops._orig_points, self.num_slices, scaling)
                 pos = list(self.ops.points)
                 self.grid_box = pg.PolyLineROI(pos, closed=True, movable=not self._refined, resizable=False,
@@ -193,7 +193,6 @@ class FIBControls(BaseControls):
             if self.show_peaks_btn.isChecked():
                 self.show_peaks_btn.setChecked(False)
                 self.show_peaks_btn.setChecked(True)
-
 
     @utils.wait_cursor('print')
     def _update_shifts(self, state):

clement/fm_operations.py
@@ -102,16 +102,18 @@ class FM_ops(Peak_finding):
                 self.print('Voxel size: ', self.voxel_size)
                 self.old_fname = fname
 
+            z = self.num_slices - 1 - z #flip z axis, now z=0 is the most upper slice
             # TODO: Look into modifying read_lif to get
             # a single Z-slice with all channels rather than all slices for a single channel
             self.orig_data = np.array([self.reader.getFrame(channel=i, dtype='u2')[z, :, :].astype('f4')
                                        for i in range(self.num_channels)])
-            self.orig_data = self.orig_data.transpose(2, 1, 0)
+            self.orig_data = self.orig_data.transpose(1, 2, 0)
             #normalize to 100
             for i in range(self.orig_data.shape[-1]):
                 self.orig_data[:, :, i] = (self.orig_data[:, :, i] - self.orig_data[:, :, i].min()) / \
                                              (self.orig_data[:, :, i].max() - self.orig_data[:, :, i].min())
                 self.orig_data[:,:,i] *= self.norm_factor
+
             self.data = np.copy(self.orig_data)
             self.selected_slice = z
             [self._aligned_channels.append(False) for i in range(self.num_channels)]
@@ -250,7 +252,7 @@ class FM_ops(Peak_finding):
             #self.max_proj_data /= self.max_proj_data.mean((0, 1))
         else:
             self.max_proj_data = np.array([self.reader.getFrame(channel=i, dtype='u2').max(0)
-                                           for i in range(self.num_channels)]).transpose(2, 1, 0).astype('f4')
+                                           for i in range(self.num_channels)]).transpose(1, 2, 0).astype('f4')
             #self.max_proj_data /= self.max_proj_data.mean((0, 1))
             for i in range(self.num_channels):
                 self.max_proj_data[:,:,i] = (self.max_proj_data[:,:,i] - self.max_proj_data[:,:,i].min()) / \
@@ -347,7 +349,8 @@ class FM_ops(Peak_finding):
         return z
 
     def load_channel(self, ind):
-        self.channel = np.array(self.reader.getFrame(channel=ind, dtype='u2').astype('f4')).transpose((2, 1, 0))
+        self.channel = np.array(self.reader.getFrame(channel=ind, dtype='u2').astype('f4')).transpose((1, 2, 0))
+        self.channel = np.flip(self.channel, axis=2) #flip z axis, z=0 is the most upper slice
         self.channel = (self.channel - self.channel.min()) / (self.channel.max() - self.channel.min())
         self.channel *= self.norm_factor
         self._channel_idx = ind

clement/peak_finding.py
@@ -218,7 +218,6 @@ class Peak_finding():
         z_profile = data[np.round(peaks_2d[:, 0]).astype(int), np.round(peaks_2d[:, 1]).astype(int)]
         mean_int = np.median(np.max(z_profile, axis=1), axis=0)
         max_int = np.max(z_profile)
-        z_max = np.argmax(z_profile, axis=1)
         x = np.arange(len(z_profile[0]))
 
         gauss = lambda x, mu, sigma, offset: mean_int * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2)) + offset
@@ -241,7 +240,7 @@ class Peak_finding():
             except RuntimeError:
                 pass
         if local:
-            return self.num_slices - 1 - popt[0]
+            return popt[0]
         else:
             if len(sigma_list) == 0:
                 self.print('Z fitting of the beads failed. Contact developers!')
@@ -262,14 +261,12 @@ class Peak_finding():
                     self.print('Unable to fit z profile. Calculate argmax(z).')
                     self.print('WARNING! Calculation of the z-position might be inaccurate!')
                     mean_values.append(np.argmax(z_profile[i]))
-                perr = np.sqrt(np.diag(pcov))[0]
-                mean_values.append(self.num_slices - 1 - popt[0])
+                mean_values.append(popt[0])
 
             if transformed:
                 self.tf_peaks_z = np.copy(mean_values)
             else:
                 self.peaks_z = np.copy(mean_values)
-                #np.save('z_values_tilted.npy', self.peaks_z)
 
     def calc_local_z(self, data, point, transformed, tf_matrix=None, flips=None, shape=None):
         if transformed:
@@ -309,7 +306,6 @@ class Peak_finding():
             return (intens * np.exp(-(x - mu_x) ** 2 / (2 * sigma_x **2)) * np.exp(-(y - mu_y) ** 2 / (2 * sigma_y ** 2)) *
                     np.exp(-(z - mu_z) ** 2 / (2 * sigma_z ** 2)) + offset).ravel()
 
-
         if channel is None:
             channel = self._channel_idx
 
@@ -324,13 +320,15 @@ class Peak_finding():
             self.print('You have to select a point within the bounds of the image!')
             return None, None, None
 
-        idx = self.check_peak_index(point, 10, False)
-        my_point = np.copy(point)
+        idx = None
+        if self.peaks is not None:
+            idx = self.check_peak_index(point[:2], 10, False)
+        point_tmp = np.copy(point)
         if idx is not None:
             peaks_2d = self.peaks[idx]
             bead = True
         else:
-            peaks_2d = my_point
+            peaks_2d = point_tmp
             bead = False
 
         roi_size = 10
@@ -342,7 +340,6 @@ class Peak_finding():
 
         if self.my_counter is None:
             self.my_counter = 0
-        #np.save('virus{}.npy'.format(self.my_counter), data)
         self.my_counter += 1
         offset = np.mean(data)
         max_proj = np.max(data, axis=-1)
@@ -384,8 +381,8 @@ class Peak_finding():
         else:
             point = self._color_matrices[channel] @ np.array([popt[0]+x_min, popt[1]+y_min, 1])
 
-        z = self.num_slices - 1 - popt[2]
-        #print('Gauss fit: ', z)
+        z = popt[2]
+        print('Gauss fit: ', z)
         init = np.array([point[0], point[1], z])
         self.log('Model fit: ', r2)
 
@@ -397,12 +394,10 @@ class Peak_finding():
         else:
             self.print('Fitting succesful: ', init, ' Uncertainty: ', perr[:3]*self.voxel_size*1e9)
 
-
         z_profile = self.channel[np.round(peaks_2d[0]).astype(int), np.round(peaks_2d[1]).astype(int)]
         z_profile = np.expand_dims(z_profile, axis=0)
         mean_int = np.median(np.max(z_profile, axis=1), axis=0)
         max_int = np.max(z_profile)
-        z_max = np.argmax(z_profile, axis=1)
         x = np.arange(len(z_profile[0]))
         gauss = lambda x, mu, sigma, offset: mean_int * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2)) + offset
 
@@ -423,7 +418,7 @@ class Peak_finding():
                     sigma_list.append(popt_z[1])
             except RuntimeError:
                 pass
-        #print('z fit: ', self.num_slices - 1 - popt_z[0])
+        print('z fit: ', popt_z[0])
 
         return init, perr[:3], pcov[:3,:3]
 